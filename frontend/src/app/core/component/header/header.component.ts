e744494caef5b847c195707234805db68fc7096e
waschndolos <killear@gmx.de>
2024-03-10 14:04:06 +0100

    rename method

--- frontend/src/app/core/component/header/header.component.ts
+++ frontend/src/app/core/component/header/header.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { ThemeService } from '../../service/theme.service';
+import { FormsModule } from '@angular/forms';
+import { NgForOf } from '@angular/common';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [],
+    imports: [FormsModule, NgForOf, SharedModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
 export class HeaderComponent {
     constructor(private themeService: ThemeService) {}
 
-    toggleMode(): void {
+    toggleTheme(): void {
         this.themeService.toggleDarkMode();
     }
 }
