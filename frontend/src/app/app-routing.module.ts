77ca0c44cdc0009bd7200b941639781a0143d631
waschndolos <killear@gmx.de>
2024-03-10 13:11:18 +0100

    First draft of a general layout

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () =>
+            import('./home/home.module').then((m) => m.HomeModule),
+    },
+    {
+        path: 'member',
+        loadChildren: () =>
+            import('./member/member.module').then((m) => m.MemberModule),
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/core/component/sidebar/sidebar.component.ts
+++ frontend/src/app/core/component/sidebar/sidebar.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgForOf } from '@angular/common';
+
+export class Link {
+    name: string;
+    link: string;
+    constructor(name: string, link: string) {
+        this.name = name;
+        this.link = link;
+    }
+}
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [RouterLink, NgForOf],
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+    activeIndex: number = 0;
 
+    links: Link[] = [];
+
+    ngOnInit(): void {
+        this.links = [new Link('Home', '/'), new Link('Members', 'member')];
+    }
+
+    updateIndex(index: number): void {
+        this.activeIndex = index;
+    }
 }
