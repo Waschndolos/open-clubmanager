2adb4beef7c7ec9193972b38e7f11d3397f43783
waschndolos <killear@gmx.de>
2024-03-10 16:18:04 +0100

    Fix tests, remove warnings

--- frontend/src/app/core/component/sidebar/sidebar.component.ts
+++ frontend/src/app/core/component/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgForOf } from '@angular/common';
+import { $localize } from '@angular/localize/init';
 
 export class Link {
     name: string;
