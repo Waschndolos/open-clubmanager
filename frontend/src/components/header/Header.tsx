cac17d3aee6d602a764dd95b15a31e3afdcef466
Waschndolos <killear@gmx.de>
2025-06-09 17:31:54 +0200

    add little padding to title

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -38,7 +38,7 @@ export default function Header() {
     return (
         <AppBar position="static" color="primary">
             <Toolbar>
-                <Typography variant="h6" sx={{flexGrow: 1}}>
+                <Typography variant="h6" sx={{flexGrow: 1, paddingLeft: 2}}>
                     Open ClubManager
                 </Typography>
 
