31f2e7437ec793b598a4ba901ece68ec69174893
Waschndolos <killear@gmx.de>
2026-02-08 17:10:57 +0100

    fix lint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,37 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import tseslint from "typescript-eslint";
+
+export default [
+    js.configs.recommended,
+
+    ...tseslint.configs.recommended,
+
+    {
+        files: ["**/*.{ts,tsx}"],
+        plugins: {
+            react,
+            "react-hooks": reactHooks,
+            "jsx-a11y": jsxA11y,
+        },
+        languageOptions: {
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        rules: {
+            "react/react-in-jsx-scope": "off",
+        },
+    },
+];
